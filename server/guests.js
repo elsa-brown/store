d8a2fe3ecc15d746d7eb566e6d8809e4decfcbae
Elsa Brown <elsambrown@gmail.com>
2017-09-18 18:24:27 -0400

    clean comments

--- server/guests.js
+++ server/guests.js
@@ -6,22 +6,13 @@ const Order = db.model('order')
 
 module.exports = require('express').Router()
   .post('/', (req, res, next) => {
-    // (What req.body is...) req.body = {orderId: orderId, guestEntry: {...}}
     const guest = req.body.guestEntry
     const orderId = +req.body.orderId
     Guest.create(guest)
-    .then(guest => {
-      return guest.setOrder(orderId) 
-    })
+    .then(guest => guest.setOrder(orderId))
     .then(() => res.sendStatus(201))
     .catch(next)
-    })
-    
-  // .get('/:id',
-  //   (req, res, next) =>
-  //     User.findById(req.params.id)
-  //     .then(user => res.json(user))
-  //     .catch(next))
+  })
   .get('/:orderId', (req, res, next) => {
     Guest.findOne({
       where: { order_id: +req.params.orderId }
